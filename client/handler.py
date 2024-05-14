493fe2de46edf437973c5e4bfaa2725c22ec3e7d
feruzbek23 <feruzbek.dev@gmail.com>
2024-05-14 17:25:23 +0200

    change code in handlers

--- client/handler.py
+++ client/handler.py
@@ -8,10 +8,10 @@ def get_user(db: Session, user_id: int):
 
 
 def get_user_by_username(db: Session, username: str):
-    return db.query(models.User).filter(models.User.username == username)
+    return db.query(models.User).filter(models.User.username == username).first()
 
 
-def get_users(db: Session, skip: int = 0, limit: int = 100):
+def get_users(db: Session, skip: int = 0, limit: int = 3):
     return db.query(models.User).offset(skip).limit(limit).all()
 
 
