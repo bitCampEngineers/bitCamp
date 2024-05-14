d97d080a033517e8e2c4043707d37d9d9f57f35c
feruzbek23 <feruzbek.dev@gmail.com>
2024-05-14 17:19:07 +0200

    add new file

---
+++ client/handler.py
@@ -0,0 +1,23 @@
+from sqlalchemy.orm import Session
+
+from . import models, schemas
+
+
+def get_user(db: Session, user_id: int):
+    return db.query(models.User).filter(models.User.id == user_id).first()
+
+
+def get_user_by_username(db: Session, username: str):
+    return db.query(models.User).filter(models.User.username == username)
+
+
+def get_users(db: Session, skip: int = 0, limit: int = 100):
+    return db.query(models.User).offset(skip).limit(limit).all()
+
+
+def create_user(db: Session, user: schemas.UserCreate): 
+    """
+    It is not completed. Researching how to hash and coming back to compelte you.
+    """
+    hashed_password = user.password
+    
